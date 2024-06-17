04b19b787d7e2634daa1c779efda5c35a67dbac5
vr <YC20230127@outlook.com>
2024-06-17 17:04:02 +0800

    Signed-off-by: vr <YC20230127@outlook.com>

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,10 +2,6 @@
   <div class="home">
     <h1>欢迎来到济南泉城风光</h1>
     <p>济南，这座历史文化名城，吸引了无数游客前来探寻其独有的城市韵味。</p>
-    <!--video controls>
-        <source src="@/assets/videos/jinan.mp4" type="video/mp4">
-        您的浏览器不支持视频播放。
-      </video-->
     <div class="search-container">
       <input type="text" v-model="searchQuery" placeholder="搜索景点或美食" />
       <button @click="search">搜索</button>
@@ -17,7 +13,11 @@
     <div class="recommendations">
       <h2>推荐景点</h2>
       <div class="cards">
-        <div class="card" v-for="(spot, index) in recommendedSpots" :key="index">
+        <div
+          class="card"
+          v-for="(spot, index) in recommendedSpots"
+          :key="index"
+        >
           <img :src="spot.image" :alt="spot.name" />
           <div class="card-content">
             <h3>{{ spot.name }}</h3>
@@ -32,7 +32,12 @@
         <li v-for="(news, index) in latestNews" :key="index">
           <a :href="news.link">{{ news.title }}</a>
           <p>{{ news.summary }}</p>
-          <el-link type="primary" v-if="index==0" href="http://localhost:8080/login">我要报名</el-link>
+          <el-link
+            type="primary"
+            v-if="index == 0"
+            href="http://localhost:8080/login"
+            >我要报名</el-link
+          >
         </li>
       </ul>
     </div>
@@ -40,7 +45,6 @@
 </template>
 
   <script>
-
 import Carousel from "../components/CarouSel.vue";
 export default {
   name: "HomePage",
@@ -49,7 +53,7 @@ export default {
   },
   data() {
     return {
-      searchQuery: '',
+      searchQuery: "",
       imagehome: [
         require("../assets/history1.png"),
         require("../assets/history2.png"),
@@ -59,71 +63,86 @@ export default {
       ],
       recommendedSpots: [
         {
-          name: '趵突泉',
-          image: require('../assets/baotuquan.png'),
-          description: '趵突泉是济南最著名的泉水之一，素有“天下第一泉”之称。'
+          name: "趵突泉",
+          image: require("../assets/baotuquan.png"),
+          description: "趵突泉是济南最著名的泉水之一，素有“天下第一泉”之称。",
         },
         {
-          name: '大明湖',
-          image: require('../assets/daminghu.png'),
-          description: '大明湖风景优美，是济南的一大自然景观，历史悠久。'
+          name: "大明湖",
+          image: require("../assets/daminghu.png"),
+          description: "大明湖风景优美，是济南的一大自然景观，历史悠久。",
         },
         {
-          name: '千佛山',
-          image: require('../assets/qianfoshan.png'),
-          description: '千佛山是济南的名山之一，拥有丰富的历史文化遗产。'
+          name: "千佛山",
+          image: require("../assets/qianfoshan.png"),
+          description: "千佛山是济南的名山之一，拥有丰富的历史文化遗产。",
         },
         {
-          name: '印象济南',
-          image: require('../assets/yinxiangjinan.png'),
-          description: '印象济南是济南有名商业街之一，国家4A景区'
-        }
+          name: "印象济南",
+          image: require("../assets/yinxiangjinan.png"),
+          description: "印象济南是济南有名商业街之一，国家4A景区",
+        },
       ],
       latestNews: [
         {
-          title: '济南泉水节即将开幕',
-          summary: '一年一度的济南泉水节即将拉开帷幕，届时将有丰富多彩的活动。敬请期待！',
-          link: 'http://www.jnqsj.cn/'
+          title: "济南泉水节即将开幕",
+          summary:
+            "一年一度的济南泉水节即将拉开帷幕，届时将有丰富多彩的活动。敬请期待！",
+          link: "http://www.jnqsj.cn/",
         },
         {
-          title: '济南美食节吸引大量游客',
-          summary: '济南美食节上，各地美食汇聚一堂，吸引了众多游客前来品尝美味。',
-          link: 'https://baijiahao.baidu.com/s?id=1801822338085660682&wfr=spider&for=pc'
+          title: "济南美食节吸引大量游客",
+          summary:
+            "济南美食节上，各地美食汇聚一堂，吸引了众多游客前来品尝美味。",
+          link: "https://baijiahao.baidu.com/s?id=1801822338085660682&wfr=spider&for=pc",
         },
         {
-          title: '济南旅游业发展势头强劲',
-          summary: '随着泉水景观和历史文化的宣传，济南的旅游业发展势头强劲。',
-          link: 'http://www.jnwljt.com/'
-        }
-      ]
+          title: "济南旅游业发展势头强劲",
+          summary: "随着泉水景观和历史文化的宣传，济南的旅游业发展势头强劲。",
+          link: "http://www.jnwljt.com/",
+        },
+      ],
     };
   },
   methods: {
     search() {
       //alert(`搜索内容：${this.searchQuery}`);
       // 在这里添加实际的搜索逻辑
-      const query = this.searchQuery.trim();//去掉空格
+      const query = this.searchQuery.trim(); //去掉空格
       if (query) {
         // 映射表，根据搜索词进行路由跳转
         const routesMap = {
-          '美食': 'food',
-          '千佛山': 'attractions',
-          '大明湖': 'attractions',
-          '趵突泉': 'attractions',
-          '历史': 'history',
-          '文化': 'culture'
+          把子肉: "food",
+          烧烤: "food",
+          美食: "food",
+          千佛山: "attractions",
+          旅游: "attractions",
+          景点: "attractions",
+          大明湖: "attractions",
+          趵突泉: "attractions",
+          历史: "history",
+          文化: "culture",
           // 可添加更多的关键词映射
-        };
-        
-        const routeName = routesMap[query];
-        if (routeName) {
-          this.$router.push("/"+routeName);
-        } else {
-          alert('未找到相关内容'); // 或者跳转到一个默认页面
         }
+        for (const key in routesMap) {
+          if (query.includes(key)) {
+            const routeName = routesMap[key];
+            if (routeName) {
+              this.$router.push("/" + routeName);
+            } else {
+              alert("未找到相关内容"); // 或者跳转到一个默认页面
+            }
+          }
+        }
+        //const routeName = routesMap[query];
+        //if (routeName) {
+        //this.$router.push("/"+routeName);
+        //} else {
+        //alert('未找到相关内容'); // 或者跳转到一个默认页面
+        //}
       }
-    }
-  }
+    },
+  },
 };
 </script>
   
@@ -171,7 +190,8 @@ video {
   border: 2px solid #ddd; /* 添加底盘以达到更好显示 */
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* 添加阴影 */
 }
-.recommendations, .latest-news {
+.recommendations,
+.latest-news {
   margin: 40px 0;
 }
 
